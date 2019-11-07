a6de0429ae645c0fd50951f57d4816bae0e43c7f
andriell <root.andriell@gmail.com>
2019-11-07 18:33:48 +0300

    v1.0.1

--- src/main/java/com/andriell/servlet/skeleton/repository/WorkerRepository.java
+++ src/main/java/com/andriell/servlet/skeleton/repository/WorkerRepository.java
@@ -3,5 +3,13 @@ package com.andriell.servlet.skeleton.repository;
 import com.andriell.servlet.skeleton.entity.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
+    // Типизированная магия. Jpa может преобразовывать методы именованные в соответствии с соглащением об именовании
+    // методов непосредственно в методы класса репозитория выполняющие необходимые sql запросы и выполняющие
+    // соответствующие преобразования в соответствующие сущьности
+    List<Worker> findByFirstName(String firstName);
+    List<Worker> findByLastName(String lastName);
+    List<Worker> findByFirstNameAndLastName(String firstName, String lastName);
 }
